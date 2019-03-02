87ff575052b7dbed2e9a07ed94b75653a11b678e
folkpark <parkerfolkman@gmail.com>
2019-03-02 12:58:15 -0700

    Test

--- Part2/sub_client.py
+++ Part2/sub_client.py
@@ -10,7 +10,7 @@ def main():
     # accept all topics (prefixed) - default is none
     socket.setsockopt_string(zmq.SUBSCRIBE, "")
     socket.connect("tcp://34.73.68.201:%s"%port)
-    print("Socket connected")
+    print("Socket connected on port: %s"%port)
     while True:
         message = socket.recv_string()
         print(message)
