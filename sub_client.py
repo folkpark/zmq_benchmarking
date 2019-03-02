88c37a4645a7bbd2149c904306d1e39fffd03f0d
folkpark <parkerfolkman@gmail.com>
2019-03-01 21:48:07 -0700

    PUB/SUB Hello World

--- sub_client.py
+++ sub_client.py
@@ -28,8 +28,4 @@ socket.setsockopt(zmq.SUBSCRIBE, topicfilter)
 total_value = 0
 for update_nbr in range (5):
     string = socket.recv()
-    topic, messagedata = string.split()
-    total_value += int(messagedata)
-    print(topic, messagedata)
-
-print("Average messagedata value for topic '%s' was %dF" % (topicfilter, total_value / update_nbr))
+    print(string)
