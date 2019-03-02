79c787e0aa0ddd461dd66b6c2204779a45a8a183
folkpark <parkerfolkman@gmail.com>
2019-03-01 22:32:25 -0700

    Test

--- sub_client.py
+++ sub_client.py
@@ -4,7 +4,7 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 # accept all topics (prefixed) - default is none
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
-socket.bind("tcp://34.73.68.201:5556")
+socket.bind("tcp://*:5556")
 
 while True:
     message = socket.recv_string()
