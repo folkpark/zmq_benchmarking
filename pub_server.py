583110785499d9b31b93feb3f120a545006d565c
folkpark <parkerfolkman@gmail.com>
2019-03-01 22:24:42 -0700

    Test

--- pub_server.py
+++ pub_server.py
@@ -1,16 +1,17 @@
 import zmq
-import random
-import sys
 import time
-
-port = "5556"
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://*:%s" % port)
+socket.connect("tcp://127.0.0.1:5556")
 
-while True:
-    topic = random.randrange(9999,10005)
-    messagedata = (b"Hello World on Topic: %s")
-    socket.send(messagedata)
+# Sleep a bit of time until the connection is properly established,
+# otherwise some messages may be lost.
+# http://stackoverflow.com/questions/7470472/lost-messages-on-zeromq-pub-sub
+time.sleep(1)
+
+for i in range(100):
+    message = "foo {}".format(i)
+    print(message)
+    socket.send_string(message)
     time.sleep(1)
--- sub_client.py
+++ sub_client.py
@@ -1,21 +1,11 @@
-import sys
 import zmq
 
-port = "5556"
-
-# Socket to talk to server
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
+# accept all topics (prefixed) - default is none
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+socket.bind("tcp://34.73.68.201:5556")
 
-print("Collecting updates from weather server...")
-socket.connect("tcp://34.73.68.201:%s" % port)
-
-# Subscribe to zipcode, default is NYC, 10001
-topicfilter = "10001"
-socket.setsockopt(zmq.SUBSCRIBE)
-
-# Process 5 updates
-total_value = 0
-for update_nbr in range (5):
-    string = socket.recv()
-    print(string)
+while True:
+    message = socket.recv_string()
+    print(message)
