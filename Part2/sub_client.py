c47c579916770e233e6e0534835b47426bd4bd1a
folkpark <parkerfolkman@gmail.com>
2019-03-02 15:16:02 -0700

    Test

--- Part2/sub_client.py
+++ Part2/sub_client.py
@@ -1,15 +1,9 @@
 import zmq
-import sys
 
 def main():
     port = 5000
-    if len(sys.argv) == 1:
-        port = sys.argv[1]
-        print("Setting port number to %s"%port)
-
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
-    # accept all topics (prefixed) - default is none
     socket.setsockopt_string(zmq.SUBSCRIBE, "")
     socket.connect("tcp://34.73.68.201:%s"%port)
     print("Socket connected on port: %s"%port)
